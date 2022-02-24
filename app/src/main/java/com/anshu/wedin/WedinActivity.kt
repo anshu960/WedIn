69bea566e5bfc5aaa8456db7373ee1280161ce2a
Anshu <anshukumarsingh448@gmail.com>
2022-02-24 11:53:53 +0530

    Initial Commit

---
+++ app/src/main/java/com/anshu/wedin/WedinActivity.kt
@@ -0,0 +1,11 @@
+package com.anshu.wedin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WedinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wedin)
+    }
+}
