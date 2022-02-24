a3a915a34bd82ddaa99f2654226a1760034e59aa
Anshu <anshukumarsingh448@gmail.com>
2022-02-24 16:28:17 +0530

    Initial Commit

--- app/src/main/java/com/anshu/wedin/LoginActivity.kt
+++ app/src/main/java/com/anshu/wedin/LoginActivity.kt
@@ -1,11 +1,54 @@
 package com.anshu.wedin
 
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import com.anshu.wedin.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var progressDialog: ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please Wait")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        binding.noAccount.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+        binding.signin.setOnClickListener {
+            validateData()
+        }
+    }
+
+    private var email = ""
+    private var password = ""
+    private fun validateData() {
+        email = binding.nameEt.editText.toString().trim()
+        password = binding.password.editText.toString().trim()
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(this, "Invalid email", Toast.LENGTH_SHORT).show()
+        } else if (password.isEmpty()) {
+            Toast.makeText(this, "Enter password", Toast.LENGTH_SHORT).show()
+        }
+        else{
+            loginUser()
+        }
+    }
+
+    private fun loginUser() {
+
     }
 }
--- app/src/main/java/com/anshu/wedin/RegisterActivity.kt
+++ app/src/main/java/com/anshu/wedin/RegisterActivity.kt
@@ -1,11 +1,98 @@
 package com.anshu.wedin
 
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import com.anshu.wedin.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var progressDialog: ProgressDialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+        firebaseAuth = FirebaseAuth.getInstance()
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please Wait")
+        progressDialog.setCanceledOnTouchOutside(false)
+        binding.button.setOnClickListener {
+          validateData()
+        }
+
+    }
+    private var name = ""
+    private var email = ""
+    private var mobile = ""
+    private var password = ""
+    private fun validateData() {
+        name = binding.username.editText.toString().trim()
+        email = binding.email.editText.toString().trim()
+        mobile = binding.mobile.editText.toString().trim()
+        password = binding.password.editText.toString().trim()
+
+        if (name.isEmpty()){
+            Toast.makeText(this, "Enter Your Name", Toast.LENGTH_SHORT).show()
+        }
+        else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            Toast.makeText(this, "Invalid Email", Toast.LENGTH_SHORT).show()
+        }
+        else if (password.isEmpty()){
+            Toast.makeText(this, "Enter Password", Toast.LENGTH_SHORT).show()
+        }
+        else if (mobile.isEmpty()){
+            Toast.makeText(this, "Enter Mobile Number", Toast.LENGTH_SHORT).show()
+        }
+        else{
+            createUserAccount()
+        }
+    }
+
+    private fun createUserAccount() {
+        progressDialog.setMessage("Creating Account")
+        progressDialog.show()
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+              updateUserInfo()
+            }
+            .addOnFailureListener { e->
+                progressDialog.dismiss()
+                Toast.makeText(this, "Failed creating account due to ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun updateUserInfo() {
+        progressDialog.setMessage("Saving user info")
+        val timestamp = System.currentTimeMillis()
+
+        val uid = firebaseAuth.uid
+        val hashMap: HashMap<String, Any?> = HashMap()
+        hashMap["uid"] = uid
+        hashMap["email"] = email
+        hashMap["name"] = name
+        hashMap["mobile"] = mobile
+        hashMap["userType"] = timestamp
+
+        val ref = FirebaseDatabase.getInstance().getReference("Users")
+        ref.child(uid!!)
+            .setValue(hashMap)
+            .addOnSuccessListener {
+                progressDialog.dismiss()
+                Toast.makeText(this, "Account created", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this@RegisterActivity, WedinActivity::class.java))
+                finish()
+            }
+            .addOnFailureListener { e->
+                progressDialog.dismiss()
+                Toast.makeText(this, "Failed saving user info due to ${e.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 }
