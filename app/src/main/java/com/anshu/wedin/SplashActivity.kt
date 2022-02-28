a5e2ccc3527a1118588cda14b53c6c3d5adf6c1d
Anshu <anshukumarsingh448@gmail.com>
2022-02-28 13:42:58 +0530

    Initial Commit

--- app/src/main/java/com/anshu/wedin/SplashActivity.kt
+++ app/src/main/java/com/anshu/wedin/SplashActivity.kt
@@ -28,6 +28,7 @@ class SplashActivity : AppCompatActivity() {
         val firebaseUser = firebaseAuth.currentUser
         if (firebaseUser == null){
             startActivity(Intent(this, MainActivity::class.java))
+            finish()
         }
         else{
 
